8f76e598e61fa5af410716fb228dc4839e94a0b0
Silas Della Contrada <me@silenium-dev.net>
2025-10-11 16:11:29 +0200

    build: add workaround for https://gitlab.kitware.com/cmake/cmake/-/issues/19409

--- native/toolchains/windows-arm64.cmake
+++ native/toolchains/windows-arm64.cmake
@@ -1,6 +1,7 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR arm64)
-set(CMAKE_GENERATOR_PLATFORM ARM64)
+# workaround for https://gitlab.kitware.com/cmake/cmake/-/issues/19409
+set(CMAKE_GENERATOR_PLATFORM ARM64 CACHE INTERNAL "")
 set(CMAKE_C_COMPILER_TARGET aarch64-windows-msvc)
 set(CMAKE_CXX_COMPILER_TARGET aarch64-windows-msvc)
 set(CMAKE_C_COMPILER clang)
--- native/toolchains/windows-x86_64.cmake
+++ native/toolchains/windows-x86_64.cmake
@@ -1,6 +1,7 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x64)
-set(CMAKE_GENERATOR_PLATFORM x64)
+# workaround for https://gitlab.kitware.com/cmake/cmake/-/issues/19409
+set(CMAKE_GENERATOR_PLATFORM x64 CACHE INTERNAL "")
 set(CMAKE_C_COMPILER_TARGET x64-windows-msvc)
 set(CMAKE_CXX_COMPILER_TARGET x64-windows-msvc)
 set(CMAKE_C_COMPILER clang)
