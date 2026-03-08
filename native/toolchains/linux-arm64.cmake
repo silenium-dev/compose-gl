0287ca06aa37a8a366969fc3d84ff991c7bb7ac9
silenium-dev <me@silenium-dev.net>
2026-03-09 00:27:11 +0100

    fix: linux-arm64 toolchain

--- native/toolchains/linux-arm64.cmake
+++ native/toolchains/linux-arm64.cmake
@@ -1,4 +1,4 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm64)
-set(CMAKE_C_COMPILER aarch64-unknown-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER aarch64-unknown-linux-gnu-g++)
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
